b5dc674a04d4b7eaeb0788ff14a99dfae74dc7d4
ASCorreia <36857753+ASCorreia@users.noreply.github.com>
2025-03-16 01:24:07 +0000

    Added test to init bill V1

--- tests/splitlana.ts
+++ tests/splitlana.ts
@@ -1,16 +1,22 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Splitlana } from "../target/types/splitlana";
+import { randomBytes } from "crypto";
 
 describe("splitlana", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.splitlana as Program<Splitlana>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const seed = new anchor.BN(randomBytes(8));
+    const tx = await program.methods.initBill(seed, new anchor.BN(10), "cenas", {sol: {}}).accounts({
+      user: provider.publicKey,
+    })
+    .rpc();
+    console.log(tx);
   });
 });
